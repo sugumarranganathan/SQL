3b608e187a09bf95eeef628c82473b74d084800e
sugumarranganathan <sugumarranganathan52@gmail.com>
2026-05-05 11:02:06 +0530

    Create create_tables.sql

---
+++ create_tables.sql
@@ -0,0 +1,25 @@
+create_tables.sql
+
+CREATE TABLE students (
+    student_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT,
+    city VARCHAR(50)
+);
+
+CREATE TABLE courses (
+    course_id INT PRIMARY KEY,
+    course_name VARCHAR(50)
+);
+
+CREATE TABLE enrollments (
+    enroll_id INT PRIMARY KEY,
+    student_id INT,
+    course_id INT,
+    FOREIGN KEY (student_id) REFERENCES students(student_id),
+    FOREIGN KEY (course_id) REFERENCES courses(course_id)
+);
+
+✅ Result:
+
+Tables created successfully
