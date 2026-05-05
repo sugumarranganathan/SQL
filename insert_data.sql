66b3fe3147b2be08797263fef708d7a42006c8aa
sugumarranganathan <sugumarranganathan52@gmail.com>
2026-05-05 11:02:56 +0530

    Create insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,21 @@
+INSERT INTO students VALUES
+(1, 'Ravi', 20, 'Chennai'),
+(2, 'Kumar', 21, 'Madurai'),
+(3, 'Anu', 19, 'Coimbatore');
+
+INSERT INTO courses VALUES
+(101, 'Math'),
+(102, 'Science'),
+(103, 'English');
+
+INSERT INTO enrollments VALUES
+(1, 1, 101),
+(2, 1, 102),
+(3, 2, 103),
+(4, 3, 101);
+
+Result:
+
+3 rows inserted (students)
+3 rows inserted (courses)
+4 rows inserted (enrollments)
